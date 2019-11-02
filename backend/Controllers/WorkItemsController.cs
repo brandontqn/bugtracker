7903633e753296f435cde9ab3a18d253b8d8334c
Brandon Nguyen <brandontqnguyen@gmail.com>
2019-11-02 16:55:38 -0700

    Added fresh new ASP.NET Core API only backend...
    For testing front end and back end separately..
    
    Will explore the angular asp.net template later.

---
+++ backend/Controllers/WorkItemsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkItemsController : ControllerBase
+    {
+        public static WorkItem[] workItems = new WorkItem[]
+        {
+            new WorkItem() { id = "1", name = "Bug 1", details = "There is a major bug!" },
+            new WorkItem() { id = "2", name = "Bug 2", details = "There is a major bug!!" },
+            new WorkItem() { id = "3", name = "Bug 3", details = "There is a major bug!!!" }
+        };
+
+        private readonly ILogger<WorkItemsController> _logger;
+
+        public WorkItemsController(ILogger<WorkItemsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WorkItem> Get()
+        {
+            return workItems;
+        }
+    }
+}
---
+++ backend/Models/WorkItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class WorkItem
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string details { get; set; }
+    }
+}
